627eeb03c06c7d651d9c40bc1fb66f1c2c1fb85b
Joonas Lehto <joonas.eemil.lehto@gmail.com>
2024-06-28 01:40:19 +0300

    Add event system for catching fish based on sector

--- archibald/scripts/FishingSystem.cs
+++ archibald/scripts/FishingSystem.cs
@@ -1,9 +1,11 @@
+using System;
 using Godot;
 
 public partial class FishingSystem : Node
 {
     [Export]
     public int SectorCount { get; set; } = 8;
+    public float DegreesPerSector => 360.0f / SectorCount;
 
     [Export]
     public Vector2I CorrectSectorWaitTime { get; set; } = new(30, 60);
@@ -11,16 +13,60 @@ public partial class FishingSystem : Node
     [Export]
     public Vector2I IncorrectSectorWaitTime { get; set; } = new(120, 240);
 
-    private int CorrectSector { get; set; } = -1;
+    [Export]
+    public bool IsDebugDrawEnabled { get; set; } = false;
+
+    [Export]
+    public float DebugSectorBallRadius { get; set; } = 1.0f;
+
+    [Export]
+    public float DebugSectorBallDistance { get; set; } = 20.0f;
+
+    [Export]
+    public float DebugSectorBallY { get; set; } = 5.0f;
+
+    public int CorrectSector { get; private set; } = -1;
 
     public void ChooseNextSector()
     {
-        CorrectSector = (int)GD.Randi() % SectorCount;
+        CorrectSector = Utils.Random(SectorCount);
+    }
+
+    public int GetSectorIndex(Vector3 position)
+    {
+        var angle = MathF.Atan2(position.Z, position.X) * 180 / MathF.PI;
+
+        // If the angle goes past 180 degrees, the returned angle is represented with negative degrees.
+        // We want the angle within [0, 360].
+        if (angle < 0)
+        {
+            angle = 360 + angle;
+        }
+
+        return (int)(angle / DegreesPerSector);
     }
 
     public override void _Ready()
     {
-        // Choose the sector when the game starts
         ChooseNextSector();
+    }
+
+    public override void _Process(double delta)
+    {
+        if (!IsDebugDrawEnabled)
+        {
+            return;
+        }
+
+        for (var sector = 0; sector < SectorCount; sector++)
+        {
+            var color = sector == CorrectSector ? Colors.Green : Colors.Red;
+            var angle = (sector * DegreesPerSector + 0.5f * DegreesPerSector) * MathF.PI / 180.0f;
+
+            var position = new Vector3(MathF.Cos(angle), DebugSectorBallY, MathF.Sin(angle));
+            position *= DebugSectorBallDistance;
+
+            DebugDraw3D.DrawSphere(position, DebugSectorBallRadius, color);
+        }
     }
 }
--- archibald/scripts/Koho.cs
+++ archibald/scripts/Koho.cs
@@ -1,44 +1,87 @@
-using Godot;
 using System;
+using Godot;
 
 public partial class Koho : RigidBody3D
 {
 	[Export]
 	public float BuoyancyRecoveryTime { get; set; } = 1.0f;
 	[Export]
-	public float WaterDrag { get; set; } = 0.85f;
-	[Export]
-	public float WaterAngularDrag { get; set; } = 0.85f;
-	[Export]
 	public float WaterLevel { get; set; } = 0.0f;
 	[Export]
 	public float BuoyancyStart { get; set; } = -5.0f;
 	[Export]
-	public Water? Water { get; set; }
-	public float Gravity { get; set; }
+	public Timer? CatchTimer { get; set; }
+
+	public event Action? Caught;
+
+	private FishingSystem? FishingSystem { get; set; }
 	private float BuoancyTimeElapsed { get; set; } = 0.0f;
 	private bool Submerged { get; set; } = false;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public void OnFishCaught()
 	{
-		Gravity = -(float)ProjectSettings.GetSetting("physics/3d/default_gravity");
-		Water = GetNode<Water>("/root/World/Water");
+		GD.Print("Catching a fish...");
+		Caught?.Invoke();
+		FishingSystem?.ChooseNextSector();
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	private void CatchFishAfter(int seconds)
 	{
+		if (CatchTimer == null)
+		{
+			GD.PrintErr("Catch timer is not set");
+			return;
+		}
+
+		GD.Print($"Catching fish in {seconds} second(s)...");
+
+		CatchTimer.WaitTime = seconds;
+		CatchTimer.Start();
+	}
+
+	private void OnSubmerged()
+	{
+		BuoancyTimeElapsed = 0.0f;
+
+		if (FishingSystem == null)
+		{
+			GD.PrintErr("Fishing system is not set");
+			return;
+		}
+
+		var sector = FishingSystem.GetSectorIndex(GlobalPosition);
+		var correct = sector == FishingSystem.CorrectSector;
+
+		if (correct)
+		{
+			GD.Print("Player threw the float in the correct sector");
+		}
+		else
+		{
+			GD.Print("Player threw the float in wrong sector");
+		}
+
+		GD.Print($"Correct Sector = {FishingSystem.CorrectSector}, Float Sector = {sector}, Angle per Sector = {FishingSystem.DegreesPerSector}");
+
+		var wait_time = correct
+			? Utils.Random(FishingSystem.CorrectSectorWaitTime)
+			: Utils.Random(FishingSystem.IncorrectSectorWaitTime);
+
+		CatchFishAfter(wait_time);
+	}
+
+	public override void _Ready()
+	{
+		FishingSystem = GetNode<FishingSystem>("/root/World/Fishing System");
+
+		if (FishingSystem == null)
+		{
+			GD.PrintErr("Failed to find the fishing system");
+		}
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		if (GlobalPosition.Y <= BuoyancyStart)
-		{
-			Submerged = true;
-			BuoancyTimeElapsed = 0.0f;
-		}
-
 		if (Submerged)
 		{
 			BuoancyTimeElapsed += (float)delta;
@@ -47,6 +90,13 @@ public partial class Koho : RigidBody3D
 			GlobalPosition = GlobalPosition.Lerp(new Vector3(GlobalPosition.X, WaterLevel, GlobalPosition.Z), buoancy);
 
 			Freeze = true;
+			return;
+		}
+
+		if (GlobalPosition.Y <= BuoyancyStart)
+		{
+			Submerged = true;
+			OnSubmerged();
 		}
 	}
 }
